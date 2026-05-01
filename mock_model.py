e6b2d97ea68e60891ac3434284d3ace0d92a3581
CauaErnani <caua.l@aluno.ifsp.edu.br>
2026-05-01 12:18:10 -0300

    Consertando erros pontuais de formatação no CI

--- mock_model.py
+++ mock_model.py
@@ -1,16 +1,17 @@
 import os
+
 import joblib
-from sklearn.dummy import DummyClassifier
 import numpy as np
+from sklearn.dummy import DummyClassifier
 
 # Cria a pasta 'models' se ela não existir
 os.makedirs('models', exist_ok=True)
 
 # Cria um modelo burro que sempre diz que NÃO é fraude (0)
-clf = DummyClassifier(strategy="constant", constant=0)
+clf = DummyClassifier(strategy='constant', constant=0)
 # Treina com dados falsos só para inicializar a estrutura (12 colunas)
 clf.fit(np.zeros((1, 12)), [0])
 
 # Agora ele vai salvar com sucesso, pois a pasta existe com certeza
-joblib.dump(clf, "models/fraud_model.pkl")
-print("Modelo mock gerado com sucesso!")
+joblib.dump(clf, 'models/fraud_model.pkl')
+print('Modelo mock gerado com sucesso!')
